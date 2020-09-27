631d909954955ab19af4439cf5b915323b4e3784
Eiwin Dev <Rohit Kumar Pal>
2020-09-27 11:08:57 +0530

    Updates

---
+++ precache-manifest.4ba625344be716d279749cecc75a036d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea8701c7d06b48822cafa4f6100bb7e",
+    "url": "/reactrecipe/index.html"
+  },
+  {
+    "revision": "7ef6260d1e505649ce4b",
+    "url": "/reactrecipe/static/css/main.5aba6e62.chunk.css"
+  },
+  {
+    "revision": "92756991f722857ec6ca",
+    "url": "/reactrecipe/static/js/2.f3f70430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactrecipe/static/js/2.f3f70430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef6260d1e505649ce4b",
+    "url": "/reactrecipe/static/js/main.ffd3ea07.chunk.js"
+  },
+  {
+    "revision": "0376811a634e41b2b9fa",
+    "url": "/reactrecipe/static/js/runtime-main.e5d523a4.js"
+  }
+]);
